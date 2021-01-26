8916a45650a1ba85871742edf853edfebb6fe4f2
Christian Garbs <mitch@cgarbs.de>
2021-01-26 21:18:55 +0100

    replace demo app with simple input form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+// import { StitchCompute } from 'stitch-compute';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stitch-compute-angular';
+  result = '';
+  input = new FormGroup({
+    from: new FormControl('1', [Validators.required, Validators.min(1), Validators.max(512)]),
+    to: new FormControl('1', [Validators.required, Validators.min(1), Validators.max(512)]),
+  });
+//  stitches = new StitchCompute();
+
+  ngOnInit(): void {
+    this.recalculate();
+  }
+
+  recalculate(): void {
+    if (this.input.status === 'VALID') {
+      this.result = '' + (this.getFrom() + this.getTo());
+    }
+    else {
+      this.result = '';
+    }
+  }
+
+  private getFrom(): number {
+    return + this.input.value.from;
+  }
+
+  private getTo(): number {
+    return + this.input.value.to;
+  }
 }
