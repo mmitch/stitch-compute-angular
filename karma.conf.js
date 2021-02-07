68954668b808d9e81f9b5a062dc3e78f9da353c8
Christian Garbs <mitch@cgarbs.de>
2021-02-07 21:07:11 +0100

    format ALL the things

--- karma.conf.js
+++ karma.conf.js
@@ -27,10 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/stitch-compute-angular'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
