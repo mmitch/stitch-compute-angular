d85eb0c7907b0c3bce78125366fecb2e253bceb1
Christian Garbs <mitch@cgarbs.de>
2021-02-09 21:15:04 +0100

    l10n: choose language at runtime based on browser settings
    locl again, this time the core library

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { getTranslations, getBrowserLang, ParsedTranslationBundle } from '@locl/core';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+const browserLang = getBrowserLang();
+const defaultLang = 'en';
+
+const pathToLang = (lang: string) => `/assets/locale/${lang}.json`;
+
+getTranslations(pathToLang(browserLang))
+  .then((data: ParsedTranslationBundle) => startup())
+  .catch(() =>
+    getTranslations(pathToLang(defaultLang))
+      .then((data: ParsedTranslationBundle) => startup())
+      .catch((err) => console.error(err))
+  );
+
+const startup = () => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+};
