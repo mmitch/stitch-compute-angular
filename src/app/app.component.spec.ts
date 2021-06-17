92e170cf9dd33ec115a5963836ed176aa7db3a36
Christian Garbs <mitch@cgarbs.de>
2021-06-17 23:32:28 +0200

    add basic Karma tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let compiled: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stitch-compute-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('stitch-compute-angular');
-  });
+  describe('Help', () => {
+    it('should not be shown initially', () => {
+      expect(compiled.querySelector('#grayout')).toBeNull();
+      expect(compiled.querySelector('#closeHelp')).toBeNull();
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('stitch-compute-angular app is running!');
+    it(`should be shown after clicking 'help'`, () => {
+      (compiled.querySelector('#openHelp') as HTMLButtonElement).click();
+      fixture.detectChanges();
+      expect(compiled.querySelector('#grayout')).not.toBeNull();
+      expect(compiled.querySelector('#closeHelp')).not.toBeNull();
+    });
+
+    it(`should be removed after clicking 'close'`, () => {
+      (compiled.querySelector('#openHelp') as HTMLButtonElement).click();
+      fixture.detectChanges();
+      (compiled.querySelector('#closeHelp') as HTMLButtonElement).click();
+      fixture.detectChanges();
+      expect(compiled.querySelector('#grayout')).toBeNull();
+      expect(compiled.querySelector('#closeHelp')).toBeNull();
+    });
+
+    it(`should be removed after clicking the help dialog box`, () => {
+      (compiled.querySelector('#openHelp') as HTMLButtonElement).click();
+      fixture.detectChanges();
+      (compiled.querySelector('#help') as HTMLButtonElement).click();
+      fixture.detectChanges();
+      expect(compiled.querySelector('#grayout')).toBeNull();
+      expect(compiled.querySelector('#closeHelp')).toBeNull();
+    });
   });
 });
