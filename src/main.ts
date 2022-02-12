2c711294c4103cdeeb2cced6e8ce9e6b9ac1e28e
Christian Garbs <mitch@cgarbs.de>
2022-02-12 21:13:12 +0100

    switch from locl to ng extract-i18n
    the locl project seems dead and does not work with newer Angular versions
    
    no automatic language detection any more, I think

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { getTranslations, getBrowserLang, ParsedTranslationBundle } from '@locl/core';
-
 if (environment.production) {
   enableProdMode();
 }
 
-const browserLang = getBrowserLang();
-const defaultLang = 'en';
-
-const pathToLang = (lang: string) => `assets/locale/${lang}.json`;
-
-getTranslations(pathToLang(browserLang))
-  .then((data: ParsedTranslationBundle) => startup())
-  .catch(() =>
-    getTranslations(pathToLang(defaultLang))
-      .then((data: ParsedTranslationBundle) => startup())
-      .catch((err) => console.error(err))
-  );
-
-const startup = () => {
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
-};
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
