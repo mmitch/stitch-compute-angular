a0117d599aa9fc0860983b42fe4133f5ac62861f
CI/CD deployment <devnull@cgarbs.de>
2023-12-14 19:57:40 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/mmitch/stitch-compute-angular/commit/ba24ca83e9147a718f82d9c3a8d8cd78b0ecf60c

--- main.js
+++ main.js
@@ -309,9 +309,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "BUILD_INFO": () => (/* binding */ BUILD_INFO)
 /* harmony export */ });
 const BUILD_INFO = {
-    commit: '465702e',
-    lastChange: '2023-12-05 19:30:55',
-    buildTime: '2023-12-05 19:32:23'
+    commit: 'ba24ca8',
+    lastChange: '2023-12-14 19:56:00',
+    buildTime: '2023-12-14 19:57:25'
 };
 
 
